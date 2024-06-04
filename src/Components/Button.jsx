871cfa4e3a38b695c7cabdebd13579f14398fbe3
amanrajwar <amanrajwar79@gmail.com>
2024-06-04 14:01:17 +0530

    special offer section is done

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 
-const Button = ({label,iconURL}) => {
+const Button = ({ label, iconURL }) => {
   return (
     <button className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red'>
-        {label}
-        <img
-        src={iconURL}
-        alt='arrow right icon'
-        className=' ml-2 rounded-full'
-        />
-        </button>
+      {label}
+      {iconURL
+        && <img
+          src={iconURL}
+          alt='arrow right icon'
+          className=' ml-2 rounded-full'
+        />}
+    </button>
+
   )
 }
 
